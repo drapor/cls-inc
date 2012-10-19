b39af7fbd071cb5fae9168603276060c4c6deb9f
Samuel Bellerose <samuel.bellerose@me.com>
2012-10-19 16:18:57 -0400

    update cours

--- Page/Admin/admin_course_add.aspx.vb
+++ Page/Admin/admin_course_add.aspx.vb
@@ -8,45 +8,6 @@ Partial Class Page_Admin_admin_course_add
         leContext = New modelCLSContainer
     End Sub
 
-    'Sub ajouterEmployeClick(sender As Object, e As EventArgs)
-    '    If Page.IsValid = True Then
-    '        Dim entEmploye As New modelCLSContainer
-    '        Dim entAdmin As New modelCLSContainer
-    '        Dim unEmploye As MembresJeu_Employe = Nothing
-    '        Dim unAdmin As MembresJeu_Admin = Nothing
-
-    '        'Dim email As String = txtCourriel.Text
-
-    '        'Dim utilisateur = (From A In entEmploye.MembresJeu Where (A.courriel = email) Select A).Any
-
-    '        'If utilisateur = Nothing Then
-
-    '        '    If dropDownType.SelectedValue = 1 Then
-    '        '        unAdmin = MembresJeu_Admin.CreateMembresJeu_Admin(0, txtNom.Text, txtPrenom.Text, txtTelephone.Text, txtMDP.Text, txtAdresse.Text, txtVille.Text, Date.Now.ToShortDateString, txtDate.Text, txtCourriel.Text, txtCodePostal.Text, 0, rdbtnSexe.SelectedItem.Value, dropDownType.SelectedValue, 0, 0)
-    '        '        entAdmin.MembresJeu.AddObject(unAdmin)
-    '        '        entAdmin.SaveChanges()
-    '        '        lblFelicitation.Text = "L'administrateur a &eacute;t&eacute; ajout&eacute; avec succ&egrave;s !"
-    '        '    Else
-    '        '        unEmploye = MembresJeu_Employe.CreateMembresJeu_Employe(0, txtNom.Text, txtPrenom.Text, txtTelephone.Text, txtMDP.Text, txtAdresse.Text, txtVille.Text, Date.Now.ToShortDateString, txtDate.Text, txtCourriel.Text, txtCodePostal.Text, 0, rdbtnSexe.SelectedItem.Value, dropDownType.SelectedValue, 0)
-    '        '        entEmploye.MembresJeu.AddObject(unEmploye)
-    '        '        entEmploye.SaveChanges()
-    '        '        lblFelicitation.Text = "L'employ&eacute; a &eacute;t&eacute; ajout&eacute; avec succ&egrave;s !"
-    '        '    End If
-
-    '        lblFelicitation.Visible = True
-    '        checkImage.Visible = True
-    '        ResetFormControlValues(Me)
-
-    '    Else
-
-    '        'lblErreurEmail.Visible = True
-    '        lblFelicitation.Visible = False
-    '        checkImage.Visible = False
-
-    '    End If
-    '    'End If
-
-    'End Sub
 
     Private Sub ResetFormControlValues(ByVal parent As Control)
         For Each c As Control In parent.Controls
--- Page/Autre/category.aspx.vb
+++ Page/Autre/category.aspx.vb
@@ -1,12 +1,37 @@
 ﻿
 Partial Class Page_Autre_category
     Inherits System.Web.UI.Page
+    Dim cours As String
+    Dim url As String
+    Dim categorie As String
+
 
     Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
         Dim categorie As String = Request.QueryString("categorie")
         dsCategorie.WhereParameters("categorie").DefaultValue = categorie
         lblCategorie.Text = categorie
+        If categorie = "Sport" Then
+            imgCategory.ImageUrl = "~/App_Themes/Original/img/sports_banner.png"
+        ElseIf categorie = "Loisir" Then
+            imgCategory.ImageUrl = "~/App_Themes/Original/img/loisir_banner.png"
+        ElseIf categorie = "Culture" Then
+            imgCategory.ImageUrl = "~/App_Themes/Original/img/culture_banner.png"
+        End If
+
     End Sub
 
 
+    Protected Sub lvCours_ItemCommand(sender As Object, e As System.Web.UI.WebControls.ListViewCommandEventArgs) Handles lvCours.ItemCommand
+
+        Dim dataItem As ListViewDataItem = DirectCast(e.Item, ListViewDataItem)
+        Dim idCours As Integer = lvCours.DataKeys(dataItem.DisplayIndex).Value.ToString()
+        Session("idCours") = idCours
+        categorie = Request.QueryString("categorie")
+        If e.CommandName = "selection" Then
+            cours = idCours
+            url = "../Autre/description.aspx?categorie=" & categorie
+            Response.Redirect(url)
+        End If
+
+    End Sub
 End Class
