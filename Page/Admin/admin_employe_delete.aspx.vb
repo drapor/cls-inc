4e93503d759728206447c7e505b502408cea8f8c
Samuel Bellerose <samuel.bellerose@me.com>
2012-11-16 14:37:08 -0500

    update delete

--- Page/Admin/admin_employe_delete.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿'Créé par Samuel Bellerose
-'Le 16 septembre 2012
-'Dernière mise à jour le 25 septembre 2012
-'Classe partielle qui supprime un employé de la BD
-
-Partial Class Page_Admin_admin_employe_delete
-    Inherits masterPage
-End Class
--- Page/Admin/admin_group_modify.aspx.vb
+++
@@ -1,9 +0,0 @@
-﻿'Créé par Francis Griggs
-'Le 16 septembre 2012
-'Dernière mise à jour le 25 septembre 2012
-'Classe partielle qui modifi les informations d'un groupe dans la BD associé à un cours
-
-Partial Class Page_Admin_admin_group_modify
-    Inherits masterPage
-
-End Class
